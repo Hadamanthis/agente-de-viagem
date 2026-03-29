3f940967d3a6f274359a2d84e06b3ca3440e3fd3
Geovane Menezes <geovanemrn@gmail.com>
2026-03-29 01:28:08 -0300

    Adicionando controle de exceção nas chamadas via ddgs e requests

--- web_search.py
+++ web_search.py
@@ -1,6 +1,8 @@
 from ddgs import DDGS
 import requests
 from bs4 import BeautifulSoup
+from requests import RequestException
+
 
 class WebSearch:
     def __init__(self, fetch_pages = 1):
@@ -9,10 +11,13 @@ class WebSearch:
     def search(self, prompt, max_results=3):
         """ Retorna a busca na web com contexto relevante a partir do prompt"""
 
-        with DDGS() as ddgs:
-            results = ddgs.text(prompt, max_results=max_results, region="br-pt")
-            if not results:
-                results = ddgs.text(prompt, max_results=max_results)
+        try:
+            with DDGS() as ddgs:
+                results = ddgs.text(prompt, max_results=max_results, region="br-pt")
+                if not results:
+                    results = ddgs.text(prompt, max_results=max_results)
+        except Exception:
+            return [f"Erro ao tentar informações online"]
 
         for i, result in enumerate(results[:self.fetch_pages]):
             result['body'] = self._fetch_page_content(result['href'])
@@ -21,8 +26,16 @@ class WebSearch:
 
     @staticmethod
     def _fetch_page_content(url):
-        response = requests.get(url, timeout=10, headers={"User-Agent": "Mozilla/5.0"})
+        try:
+            response = requests.get(url, timeout=10, headers={"User-Agent": "Mozilla/5.0"})
+            response.raise_for_status()
+        except RequestException as e:
+            return f"Erro ao acessar a página: {url}"
+
         soup = BeautifulSoup(response.text, "html.parser")
+
+        # Removendo tags de estilo e scripts
         for tag in soup(["script", "style"]):
             tag.decompose()
+
         return soup.get_text(separator=" ", strip=True)[:3000]
